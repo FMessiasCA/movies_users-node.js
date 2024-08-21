b9b7aa911f30be36c39770acbcefa266a61ddf65
Messias <fmessiasca@gmail.com>
2024-08-21 17:18:50 -0400

    Add create tables, migrations, DML,  registration and encryption

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,14 +1,57 @@
+const { hash } = require("bcryptjs")
+
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UserController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if (!name) {
-            throw new AppError(`Você não informou o nome.`);
-        }
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-        response.status(201).json({ name, email, password });
+        if (checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.");
+        };
+
+        const hashedPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ? ,?)",
+            [name, email, hashedPassword]);
+
+        return response.status(201).json();
+    }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado.");
+        };
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este e-mail já pertence a outro usuário.")
+        };
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(
+            `UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
     }
 };
 
--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -2,6 +2,13 @@ const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
+/* 
+O processo de conexão com o banco de dados leva em consideração alguns fatores ao iniciar, como;
+-> Já existe esse arquivo? 
+    * Se sim, ele é iniciado. 
+    * Senão, o arquivo é criado. (O que é o caso quando ele é iniciado a primeira vez)
+Por não acontecer no mesmo momento é necessário usar uma função assíncrona.
+*/
 async function sqliteConnection() {
     const database = await sqlite.open({
         filename: path.resolve(__dirname, "..", "database.db"),
